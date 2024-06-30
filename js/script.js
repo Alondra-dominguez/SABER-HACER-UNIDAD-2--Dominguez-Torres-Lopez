c2c30109e9cbbf69ea189e650c8a919fee9924bc
CesarLopez02 <patrickgarcia862@gmail.com>
2024-06-29 20:25:40 -0600

    Cambios en About

--- js/script.js
+++ js/script.js
@@ -134,10 +134,10 @@
             new Product(2, 'Perfect Sense', 'https://i5.walmartimages.com.mx/mg/gm/3pp/asr/e896395b-555f-4c17-84ed-9a665a5f4deb.1fef573418b1053bf5ed76f6489366d1.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 18, true, 20),
             new Product(3, 'Perfect Sense', 'https://i5.walmartimages.com.mx/mg/gm/3pp/asr/c8c39c45-0f4c-4c4c-bf8c-31cf7488ba80.62cff588bffa86c442febb854ee56c35.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 25, true, 50),
             new Product(4, 'Perfect Sense', 'https://i5.walmartimages.com.mx/mg/gm/3pp/asr/1691761b-715b-49ef-9e56-814324ffa345.963d8e4a5673f3f3b0aca9fe87a023f2.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 40, false),
-            new Product(5, 'Fancy Product', 'https://i5.walmartimages.com.mx/mg/gm/1p/images/product-images/img_large/00750200287277l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 120),
-            new Product(6, 'Sale Item', 'https://i5.walmartimages.com.mx/mg/gm/1p/images/product-images/img_large/00070646002387l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 25, true, 50),
-            new Product(7, 'Special Item', 'https://i5.walmartimages.com.mx/mg/gm/1p/images/product-images/img_large/00750200287128l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 18, true, 20),
-            new Product(8, 'Popular Item', 'https://i5.walmartimages.com.mx/mg/gm/3pp/asr/449165b0-54e6-41bf-b1da-a6785402f49b.0afda167ac723721d94133088f4b171b.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 40)
+            new Product(5, 'Ganador', 'https://i5.walmartimages.com.mx/mg/gm/1p/images/product-images/img_large/00750200287277l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 120),
+            new Product(6, 'Pedigree', 'https://i5.walmartimages.com.mx/mg/gm/1p/images/product-images/img_large/00070646002387l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 25, true, 50),
+            new Product(7, 'Ganador Premium', 'https://i5.walmartimages.com.mx/mg/gm/1p/images/product-images/img_large/00750200287128l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 18, true, 20),
+            new Product(8, 'Nucan', 'https://i5.walmartimages.com.mx/mg/gm/3pp/asr/449165b0-54e6-41bf-b1da-a6785402f49b.0afda167ac723721d94133088f4b171b.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 40)
         ];
 
         // Renderizar los productos en el DOM
